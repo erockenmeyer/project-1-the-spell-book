bd03d0d2a64826ed72f55c94955bcf4de7b1b541
erockenmeyer <erikrockenmeyer@gmail.com>
2022-03-20 17:33:29 -0500

    added search history function and set up spell list display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+var formEl = document.querySelector("#spell-search");
+var searchTypeEl = document.querySelector("#dropdown-menu2");
+var spellTypeEl = document.querySelector("#username");
+var historyEl = document.querySelector("#search-history");
+var searchListEl = document.querySelector("#search-list");
+var searchHistory = [];
+var searchCounter = 0;
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get type from input
+    // var keyword = spellTypeEl.value.trim();
+    var searchType = searchTypeEl.value();
+
+    if (keyword && searchType) {
+        var historyObj = {
+            type: searchType,
+            keyword: keyword
+        };
+        displayHistory(historyObj);
+        getSpellList(searchType, keyword);
+    }
+
+    console.log(searchType);
+}
+
 var getSpellList = function(type, keyword) {
 
     // checks for multiple types
@@ -31,10 +58,89 @@ var getSpellList = function(type, keyword) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                displayList(data);
             })
         } else {
             console.log("Something went wrong!");
         }
     })
 }
+
+function displayList(data) {
+    // assign list to variable
+    var spellList = data.results;
+
+    // check that there are results
+    if (spellList.length === 0) {
+        searchListEl.textContent = "No spells found!"
+        return;
+    }
+
+    // clear any old content
+    searchListEl.textContent = "";
+
+    // create container in search list div
+    var listContainer = document.createElement("ul");
+
+    // iterate through list
+    for (var i = 0; i < spellList.length; i++) {
+        var name = spellList[i].name;
+        var url = spellList[i].index;
+
+        // make list els to contain spell info
+        var listEl = document.createElement("li");
+        var urlEl = document.createElement("a");
+        urlEl.setAttribute("href", "./spell-finder.html?spell=" + url);
+        urlEl.textContent = name;
+
+        // append to container
+        listEl.appendChild(urlEl);
+        listContainer.appendChild(listEl);
+    }
+    console.log(spellList[0]);
+
+    // append container to dom
+    searchListEl.appendChild(listContainer);
+}
+
+var displayHistory = function(historyObj) {
+    var historyItemEl = document.createElement("li");
+
+    // add counter as custom attribute
+    historyItemEl.setAttribute("data-search-id", searchCounter);
+
+    // display type and search word
+    historyItemEl.innerHTML = "<span>" + historyObj.type + "</span> <span>" + historyObj.keyword + "</span>";
+
+    historyObj.id = searchCounter;
+    searchHistory.push(historyObj);
+    saveSearch();
+
+    historyEl.appendChild(historyItemEl);
+
+    // increase count
+    searchCounter++;
+}
+
+// saves history to local
+var saveSearch = function() {
+    localStorage.setItem("search history", JSON.stringify(searchHistory));
+}
+
+var loadHistory = function() {
+    var history = localStorage.getItem("search history");
+    if (!history) {
+        return false;
+    }
+
+    // convert to array & display
+    history = JSON.parse(history);
+    for (var i = 0; i < history.length; i++) {
+        displayHistory(history);
+    }
+}
+
+getSpellList("level", "2");
+
+formEl.addEventListener("submit", formSubmitHandler);
+loadHistory();
