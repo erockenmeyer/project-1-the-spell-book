f964a2f889743faab0c99186ccb5b7759916ffdf
Anh Vuong <vta19797@gmail.com>
2022-03-22 19:19:53 -0500

    get dice result

--- assets/js/dice-roll.js
+++ assets/js/dice-roll.js
@@ -1,29 +1,47 @@
 
 var diceTypeEl=document.querySelector('#dice-type');
-var resultEl=$('#result');
+var resultEl=document.querySelector('#result');
+var rollDiceBtnEl=document.querySelector('.btn');
 var chosenType;
 
-
-diceTypeEl.addEventListener('change',()=>{
+//choose dice type for api 
+rollDiceBtnEl.addEventListener('click',()=>{
     chosenType=diceTypeEl.options[diceTypeEl.selectedIndex].text;
-   
-});
-var getDiceApi =function(){
-  
-    var diceURL="http://roll.diceapi.com/json/" + chosenType;
     
-    fetch(diceURL).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-            })
-        } else {
-            alert("Something went wrong!");
-        }
-    })
-    .catch(function(error){
-        alert('Unable to connect to Dice Api');
-    });
+
+    var getDiceApi =function(){
+  
+        var diceURL="http://roll.diceapi.com/json/" + chosenType;
+    
+            fetch(diceURL).then(function(response) {
+                if (response.ok) {
+                    response.json().then(function(data) {
+                   console.log(data);
+                   getDiceResult(data);
+                  
+                })
+                } else {
+                    alert("Something went wrong!");
+                }
+             })
+            .catch(function(error){
+                alert('Unable to connect to Dice Api');
+            });
 };
+getDiceApi();
+
+});
+var getDiceResult=function(data){
+    
+    var result=data.dice[0].value;
+    console.log(result);
+    resultEl.innerHTML=result;
+
+
+
+}
+
+
+
 
 
