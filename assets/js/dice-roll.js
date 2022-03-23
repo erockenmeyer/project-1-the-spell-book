ce397121d17ed96380b878f8fe5454502f826749
Anh Vuong <vta19797@gmail.com>
2022-03-22 23:08:56 -0500

    add function to roll 1 dice multiple times

--- assets/js/dice-roll.js
+++ assets/js/dice-roll.js
@@ -3,20 +3,22 @@ var diceTypeEl=document.querySelector('#dice-type');
 var resultEl=document.querySelector('#result');
 var rollDiceBtnEl=document.querySelector('.btn');
 var chosenType;
+var rollTimes=document.querySelector('#times');
 
 //choose dice type for api 
 rollDiceBtnEl.addEventListener('click',()=>{
+   
     chosenType=diceTypeEl.options[diceTypeEl.selectedIndex].text;
     
-
+if(rollTimes.value>=1){
     var getDiceApi =function(){
   
-        var diceURL="http://roll.diceapi.com/json/" + chosenType;
+        var diceURL="http://roll.diceapi.com/json/" + rollTimes.value+chosenType;
     
             fetch(diceURL).then(function(response) {
                 if (response.ok) {
                     response.json().then(function(data) {
-                   
+                   console.log(diceURL);
                    getDiceResult(data);
                   
                 })
@@ -29,18 +31,36 @@ rollDiceBtnEl.addEventListener('click',()=>{
             });
 };
 getDiceApi();
+}
+else{
+    alert('please pick a number that is greater than 1');
+}
 
 });
 var getDiceResult=function(data){
+    for (var i=0; i<rollTimes.value;i++){
+    var result=data.dice[i].value;
     
-    var result=data.dice[0].value;
-    
-    resultEl.innerHTML=result;
+    //resultEl.innerHTML=result;
+    //create a container for each dice result
+        var containerEl=document.createElement("div");
+        //can put class list here
+        //containEl.classList=
+    //create span element to hold the result
+        var resultSpanEl=document.createElement("span");
+        resultSpanEl.textContent=result;
+    //append container
+        containerEl.appendChild(resultSpanEl);
+    //append container to the DOM
+        resultEl.appendChild(containerEl);
 
-
+    }
 
 }
 
+function refreshPage(){
+    window.location.reload();
+} 
 
 
 
